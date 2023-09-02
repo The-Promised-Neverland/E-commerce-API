831ebd5240c85c503139bf425665bba66e6342ad
Abhijit Roy <133197042+The-Promised-Neverland@users.noreply.github.com>
2023-09-02 12:36:14 +0530

    Update server.js

--- server.js
+++ server.js
@@ -25,7 +25,7 @@ const app = express();
 
 // Enable CORS with credentials support
 app.use(cors({
-  origin: ['https://playtech-six.vercel.app', 'http://localhost:3000'], // You can replace this with the specific origins you want to allow
+  origin: ['https://playtech-mu.vercel.app', 'http://localhost:3000'], // You can replace this with the specific origins you want to allow
   credentials: true, // Enable credentials support (cookies, HTTP authentication, etc.)
 }));
 
